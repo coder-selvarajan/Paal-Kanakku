20530ac76327e794b9995fe9fc25b8a037fdfe38
Selvarajan Thangavel <selvarajan.thangavel@d4t4solutions.com>
2021-06-30 22:57:40 +0530

    Initial Commit

---
+++ Paal Kanakku/Paal_KanakkuApp.swift
@@ -0,0 +1,20 @@
+//
+//  Paal_KanakkuApp.swift
+//  Paal Kanakku
+//
+//  Created by Selvarajan on 30/06/21.
+//
+
+import SwiftUI
+
+@main
+struct Paal_KanakkuApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
